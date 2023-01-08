e3e0a373996785e6a47f86f1f8b50b163827d1f3
Sungho95 <tjdgh1785@gmail.com>
2023-01-08 14:31:13 +0900

    Initial commit

---
+++ src/test/java/deploytest/indiego/IndiegoApplicationTests.java
@@ -0,0 +1,13 @@
+package deploytest.indiego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndiegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
